2938fe914480677101748c145c5bd1a065cc17d5
Max <maximilian9219@gmail.com>
2021-08-26 12:42:08 +0200

    created newExpanse.dart file

---
+++ lib/src/widgets/newExpanse.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewExpanse extends StatefulWidget {
+  const NewExpanse({Key? key}) : super(key: key);
+
+  @override
+  _NewExpanseState createState() => _NewExpanseState();
+}
+
+class _NewExpanseState extends State<NewExpanse> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
